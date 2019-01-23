5f1ad02b40bee80d20b9996152f91d4652248114
lesteryou <lester0x05@gmail.com>
2019-01-23 09:53:38 +0800

    Init

---
+++ src/Hello/Man.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lester
+ * Date: 2019/1/23 9:35
+ * Desc:
+ */
+
+namespace Hello;
+
+
+class Man
+{
+    public function say()
+    {
+        echo 'Hello Word!';
+    }
+}
